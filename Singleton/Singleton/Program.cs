0ff888ec36716595a07c046c278be96850d965b3
Vanderberg Nunes <vanderberg.nunes@softplan.com.br>
2021-05-26 20:09:54 -0300

    first commit

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton jogador1 = Singleton.GetInstancia;
+            jogador1.Mensagem("Jogador 1: A bola está comigo!");
+            
+            Singleton jogador2 = Singleton.GetInstancia;
+            jogador2.Mensagem("Jogador 2: Recebeu a bola");
+            
+            Singleton jogador3 = Singleton.GetInstancia;
+            jogador3.Mensagem("Jogador 3: Recebeu o lancemento");
+
+            Console.ReadKey();
+        }
+    }
+}
