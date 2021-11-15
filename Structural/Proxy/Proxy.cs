31cc48233b2565a7a9ff0c3d7daa3d889cfea86b
Vanderberg Nunes <vanderberg.nunes@softplan.com.br>
2021-11-15 15:56:36 -0300

    Criado projeto Proxy

---
+++ Structural/Proxy/Proxy.cs
@@ -0,0 +1,17 @@
+﻿namespace Proxy
+{
+    public class Proxy : Subject
+    {
+        private RealSubject realSubject;
+        
+        public override void Requisicao()
+        {
+            if (this.realSubject == null)
+            {
+                this.realSubject = new RealSubject();
+            }
+            
+            this.realSubject.Requisicao();
+        }
+    }
+}
