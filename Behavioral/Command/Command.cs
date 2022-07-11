66fc06a3f0b8ed333c23039c8a6b84d31b23e84a
Vanderberg Nunes <vanderberg.nunes@softplan.com.br>
2022-07-11 18:48:11 -0300

    Implementado novso Desing Patterns

---
+++ Behavioral/Command/Command.cs
@@ -0,0 +1,14 @@
+﻿namespace Command
+{
+    public abstract class Command
+    {
+        protected Receiver _receiver;
+
+        public Command(Receiver receiver)
+        {
+            this._receiver = receiver;
+        }
+
+        public abstract void Execute();
+    }
+}
