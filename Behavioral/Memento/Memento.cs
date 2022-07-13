2d2db210c80e97a9ae0723b8c5a09525e5264ba3
Vanderberg Nunes <vanderberg.nunes@softplan.com.br>
2022-07-12 21:02:30 -0300

    Criado Memento

---
+++ Behavioral/Memento/Memento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Memento
+{
+    public class Memento
+    {
+        private string _state;
+
+        public Memento(string state)
+        {
+            _state = state;
+        }
+
+        public string State
+        {
+            get => _state;
+        }
+    }
+}
---
+++ Behavioral/Memento/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Memento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Originator originator = new Originator();
+            originator.State = "ON";
+            
+            Caretaker caretaker = new Caretaker();
+            caretaker.Memento = originator.CreateMemento();
+
+            originator.State = "OFF";
+            originator.SetMemento(caretaker.Memento);
+        }
+    }
+}
