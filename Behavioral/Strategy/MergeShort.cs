00dde5d39f709f2470611a1718db7cf008ed6c41
Vanderberg Nunes <vanderberg.nunes@softplan.com.br>
2022-07-16 15:50:01 -0300

    Criado Strategy

---
+++ Behavioral/Strategy/MergeShort.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Strategy
+{
+    public class MergeShort : SortStrategy
+    {
+        public override void Sort(List<string> list)
+        {
+            Console.WriteLine("Merge sort list ");
+        }
+    }
+}
---
+++ Behavioral/Strategy/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList studantsRecords = new SortedList();
+            
+            studantsRecords.Add("Rodrigo");
+            studantsRecords.Add("Maria");
+            studantsRecords.Add("João");
+            studantsRecords.Add("Carlos");
+            studantsRecords.Add("Ana");
+            
+            studantsRecords.SetSortStrategy(new QuickSort());
+            studantsRecords.Sort();
+            
+            studantsRecords.SetSortStrategy(new ShellSort());
+            studantsRecords.Sort();
+            
+            studantsRecords.SetSortStrategy(new MergeShort());
+            studantsRecords.Sort();
+        }
+    }
+}
