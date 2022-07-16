cbdd023fcd34c232e888b95898b5c4906e3961de
Vanderberg Nunes <vanderberg.nunes@softplan.com.br>
2022-07-16 15:17:13 -0300

    Criado State

---
+++ Behavioral/State/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace State
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Context context = new Context(new ConcreteStateA());
+            context.Request();
+            context.Request();
+            context.Request();
+            context.Request();
+            
+            
+        }
+    }
+}
